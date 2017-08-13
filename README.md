b3e316eb306b91fc10d30095a1bb8ac99ba8936d
Rhys Turner <rhys.turner@imagination.com>
2017-08-13 20:32:19 +0800

    add: readme and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mautic-form
+
+This module injects a mautic genrated form into an angular page head.
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-exports.LoadForm = function(_src, _domain, _msg = "Submit successfully") {
-  console.log("This is a message from the demo package");
-  if (typeof MauticSDKLoaded == 'undefined') {
-    var MauticSDKLoaded = true;
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = _src;
-    script.onload = function () {
-      MauticSDK.onLoad();
-      console.log("MauticSDK.onLoad");
-    };
-    head.appendChild(script);
-    var MauticDomain = _domain;
-    var MauticLang = {
-      'submittingMessage': _msg
+/**
+ * Load mautic form into page.
+ *
+ * @param  {String} _src
+ * @param  {String} _domain
+ * @param  {String} _msg
+ */
+module.exports = {
+  LoadForm: function (_src, _domain, _msg = "success") {
+    if (typeof MauticSDKLoaded == 'undefined') {
+      var MauticSDKLoaded = true;
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = _src;
+      script.onload = function () {
+        MauticSDK.onLoad();
+        console.log("MauticSDK.onLoad");
+      };
+      head.appendChild(script);
+      var MauticDomain = _domain;
+      var MauticLang = {
+        'submittingMessage': _msg
+      }
     }
   }
-}
+};
